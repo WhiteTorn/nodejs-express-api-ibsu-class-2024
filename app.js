5ba068cd41d1d20a59aa38a40695439f76d5d5bc
WhiteTorn <127073843+WhiteTorn@users.noreply.github.com>
2024-06-28 18:21:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const studentsRouter = require('./routes/students');
 const filesRouter = require('./routes/files');
+const productRoutes = require('./routes/product');
 
 var app = express();
 
